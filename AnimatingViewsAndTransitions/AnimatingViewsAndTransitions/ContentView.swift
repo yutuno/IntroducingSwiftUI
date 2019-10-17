2afa9b10dc0be00f4ec22c0b66e19b7933b5c9b8
yuto <yuto@yuto-MacBook-Pro.local>
2019-10-17 22:46:57 +0900

    Add AnimationViewsAndTransitions Project

---
+++ AnimatingViewsAndTransitions/AnimatingViewsAndTransitions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatingViewsAndTransitions
+//
+//  Created by yuto on 2019/10/17.
+//  Copyright © 2019 yutuno. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
